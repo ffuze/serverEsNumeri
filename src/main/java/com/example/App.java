d25b141c513280e52817dc42d59668b6b1e03adb
lai2-24 <lai2-24@itismeucci.com>
2023-10-11 12:44:33 +0200

    finished server without bug fixes

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,7 +1,11 @@
 package com.example;
 
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Random;
 
 /**
  * Hello world!
@@ -17,6 +21,28 @@ public class App
             ServerSocket server = new ServerSocket(3000);
             Socket s = server.accept();
 
+            System.out.println("Client connesso");
+
+            BufferedReader in = new BufferedReader(new InputStreamReader(s.getInputStream()));
+            DataOutputStream out = new DataOutputStream(s.getOutputStream());
+            
+            out.writeBytes("Benvenuto, indovina un numero da 1 a 100");
+
+            int n = (int)(Math.random()*100+1);
+
+            int nRicevuto = Integer.parseInt(in.readLine());
+
+            while(nRicevuto != n){
+                if(nRicevuto < n)
+                    out.writeByte(1 + '\n');
+                else
+                    out.writeByte(2 + '\n');
+            }
+
+            out.writeByte(3 + '\n');
+
+            s.close();
+            server.close();
             
         } catch(Exception e){
             System.out.println(e.getMessage());
