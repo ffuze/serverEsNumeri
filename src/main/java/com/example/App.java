38ddabdf9f5e2ac066a123366511a0d276a887b4
alek <deglinnocenti.alex@itismeucci.com>
2023-10-14 15:00:39 +0200

    changes applied to server source code

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -5,7 +5,6 @@ import java.io.DataOutputStream;
 import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.Random;
 
 /**
  * Hello world!
@@ -15,7 +14,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        try{
+        try {
             System.out.println("Server in avvio...");
 
             ServerSocket server = new ServerSocket(3000);
@@ -25,29 +24,35 @@ public class App
 
             BufferedReader in = new BufferedReader(new InputStreamReader(s.getInputStream()));
             DataOutputStream out = new DataOutputStream(s.getOutputStream());
-            
-            out.writeBytes("Benvenuto, indovina un numero da 1 a 100");
 
-            int n = (int)(Math.random()*100+1);
+            out.writeBytes("Benvenuto, indovina un numero da 1 a 100" + "\n");
 
-            int nRicevuto = Integer.parseInt(in.readLine());
+            int n = (int) (Math.random() * 100 + 1);
+            System.out.println(n);
 
-            while(nRicevuto != n){
-                if(nRicevuto < n)
-                    out.writeByte(1 + '\n');
-                else
-                    out.writeByte(2 + '\n');
-            }
-
-            out.writeByte(3 + '\n');
+            int nRicevuto;
+            do {
+                nRicevuto = Integer.parseInt(in.readLine());
+                if (nRicevuto == 1) {
+                    out.writeByte(1);
+                    out.writeByte('\n');
+                } else if (nRicevuto == 2) {
+                    out.writeByte(2);
+                    out.writeByte('\n');
+                } else {
+                    out.writeByte(3);
+                    out.writeByte('\n');
+                }
+                out.flush();
+            } while (nRicevuto != 3);
 
             s.close();
             server.close();
-            
-        } catch(Exception e){
+        } catch (Exception e) {
             System.out.println(e.getMessage());
-            System.out.println("C'e' stato un errore nella fase di connessione");
+            System.out.println("C'è stato un errore nella fase di connessione");
             System.exit(1);
         }
+
     }
 }
