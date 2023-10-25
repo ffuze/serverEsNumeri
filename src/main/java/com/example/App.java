aa3fd7b9b6d1ddc8948aa9c263d58cda768e8a25
lai2-24 <lai2-24@itismeucci.com>
2023-10-25 13:40:10 +0200

    bugs almost fixed

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -10,12 +10,13 @@ import java.net.Socket;
 public class App {
     public static void main(String[] args) {
         try {
+            ServerSocket server = new ServerSocket(6969);
             do {
-                ServerSocket server = new ServerSocket(3000);
                 Socket s = server.accept();
                 MioThread t = new MioThread(s);
+                t.start();
             } while (true);
-
+            
         } catch (Exception e) {
             System.out.println(e.getMessage());
             System.out.println("Errore durante la creazione del server");
--- src/main/java/com/example/MioThread.java
+++ src/main/java/com/example/MioThread.java
@@ -6,52 +6,42 @@ import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
 
-public class MioThread extends Thread{
+    public class MioThread extends Thread{
 
-    Socket s;
+        Socket s;
 
-    public MioThread(Socket s){
-        this.s = s;
-    }
-    
-    public void run(){
-        try {
-            System.out.println("Server in avvio...");
-
-            ServerSocket server = new ServerSocket(3000);
-            Socket s = server.accept();
-
-            System.out.println("Client connesso");
-
-            BufferedReader in = new BufferedReader(new InputStreamReader(s.getInputStream()));
-            DataOutputStream out = new DataOutputStream(s.getOutputStream());
-
-            out.writeBytes("Benvenuto, indovina un numero da 1 a 100" + "\n");
-
-            int n = (int) (Math.random() * 100 + 1);
-
-            int nRicevuto = 0;
-
-            do {
-                MioThread t = new MioThread(s);
-                t.start();
-
-                nRicevuto = Integer.parseInt(in.readLine());
-                if (nRicevuto < n)
-                    out.writeBytes("1\n");
-                else if (nRicevuto > n)
-                    out.writeBytes("2\n");
-                else if (nRicevuto == n)
-                    out.writeBytes("3\n");
-            } while (nRicevuto != 3);
-
-            s.close();
-            server.close();
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-            System.out.println("C'è stato un errore nella fase di connessione oppure il client si e' disconnesso");
-            System.exit(1);
+        public MioThread(Socket s){
+            this.s = s;
         }
+        
+        public void run(){
+            try {
 
+                BufferedReader in = new BufferedReader(new InputStreamReader(s.getInputStream()));
+                DataOutputStream out = new DataOutputStream(s.getOutputStream());
+
+                int n = (int) (Math.random() * 100 + 1);
+
+                System.out.println(n);
+
+                int nRicevuto = 0;
+
+                do {
+                    nRicevuto = Integer.parseInt(in.readLine());
+                    if (nRicevuto < n)
+                        out.writeBytes("1\n");
+                    else if (nRicevuto > n)
+                        out.writeBytes("2\n");
+                    else if (nRicevuto == n)
+                        out.writeBytes("3\n");
+                } while (nRicevuto != 3);
+
+                //s.close();
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+                System.out.println("C'è stato un errore nella fase di connessione oppure il client si e' disconnesso");
+                System.exit(1);
+            }
+
+        }
     }
-}
