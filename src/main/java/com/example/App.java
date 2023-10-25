9bcaaa4c695a314cc5a0890ea5e8ea70ce14c523
lai2-24 <lai2-24@itismeucci.com>
2023-10-25 12:41:13 +0200

    added thread code to server2
    exit

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,8 +1,5 @@
 package com.example;
 
-import java.io.BufferedReader;
-import java.io.DataOutputStream;
-import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -10,44 +7,19 @@ import java.net.Socket;
  * Hello world!
  *
  */
-public class App
-{
-    public static void main( String[] args )
-    {
+public class App {
+    public static void main(String[] args) {
         try {
-            System.out.println("Server in avvio...");
-
-            ServerSocket server = new ServerSocket(3000);
-            Socket s = server.accept();
-
-            System.out.println("Client connesso");
-
-            BufferedReader in = new BufferedReader(new InputStreamReader(s.getInputStream()));
-            DataOutputStream out = new DataOutputStream(s.getOutputStream());
-
-            out.writeBytes("Benvenuto, indovina un numero da 1 a 100" + "\n");
-
-            int n = (int) (Math.random() * 100 + 1);
-
-            int nRicevuto = 0;
-            
             do {
-                nRicevuto = Integer.parseInt(in.readLine());
-                if (nRicevuto < n) 
-                    out.writeBytes("1\n");
-                else if (nRicevuto > n) 
-                    out.writeBytes("2\n");
-                else if(nRicevuto == n)
-                    out.writeBytes("3\n");
-            } while (nRicevuto != 3);
+                ServerSocket server = new ServerSocket(3000);
+                Socket s = server.accept();
+                MioThread t = new MioThread(s);
+            } while (true);
 
-            s.close();
-            server.close();
         } catch (Exception e) {
             System.out.println(e.getMessage());
-            System.out.println("C'è stato un errore nella fase di connessione oppure il client si e' disconnesso");
+            System.out.println("Errore durante la creazione del server");
             System.exit(1);
         }
-
     }
 }
